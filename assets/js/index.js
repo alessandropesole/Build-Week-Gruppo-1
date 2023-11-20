f49ed3cb1a1113d3a8a7df30708116071de1dd82
Dagniele24 <danielelazzarano@gmail.com>
2023-11-20 14:43:07 +0100

    asd

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+const btnProceed = document.getElementById('proceed');
+const checkbox = document.getElementById('check');
+
+function enableButton() {
+  if (checkbox.checked) {
+    btnProceed.disabled = false;
+  } else {
+    btnProceed.disabled = true;
+  }
+}
+
+
